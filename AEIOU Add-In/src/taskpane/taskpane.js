0ab21c51ac56d61e27cb453db42e790e8feceb67
Ho0M4n <pyenzous@gmail.com>
2022-08-05 01:16:14 +0800

    ml wip

--- AEIOU Add-In/src/taskpane/taskpane.js
+++ AEIOU Add-In/src/taskpane/taskpane.js
@@ -25,28 +25,31 @@ export async function run() {
     function callback(result) {
         document.getElementById("item-subject").innerHTML = "<b>Subject:</b> <br/>" + result.value;
         
+
         // Put in model to use the result.value
         var input = result.value
-
+        
         const spawn = require('child_process').spawn;
-        const script = spawn('python', ['./ml.py', input.toString()]);
-
-        var datatoSend 
+        const script = spawn('py', ['ml.py', input]);
+        console.log(input)
 
         script.stdout.on('data', (data) => {
-          datatoSend = data.toString();
+          // datatoSend = data.toString();
+          console.log(`${data}`)
+        });
+        script.stderr.on('data', (data) => {
+          // As said before, convert the Uint8Array to a readable string.
+          console.error(`stderr: ${data}`);
         });
 
-
-        // one of the ways to pass parameters into the py file if the other one doesnt work
-        // var data = result.value
+        // // one of the ways to pass parameters into the py file if the other one doesnt work
         // script.stdin.write(data);
         // // End data write
         // script.stdin.end();
 
-        script.on('exit', (code) => {
+        script.on('close', (code) => {
           console.log("Process quit with code : " + code);
-          res.send(datatoSend);
+          // res.send(datatoSend);
         });
 
         //
