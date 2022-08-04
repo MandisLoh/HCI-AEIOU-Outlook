2ae183446fcfed946cbf2661945c575e2ff66e3d
Ho0M4n <pyenzous@gmail.com>
2022-08-05 06:02:04 +0800

    testscript for ml is working

--- AEIOU Add-In/ml copy.py
+++ AEIOU Add-In/ml copy.py
@@ -1,16 +1,28 @@
-from sklearn.feature_extraction.text import CountVectorizer
+from sklearn.feature_extraction.text import TfidfVectorizer
+from sklearn.decomposition import PCA
 import pickle
 import sys
+import pandas as pd
+# kekw
 
 print('helo')
+print(str(sys.argv[1]))
 
 
-model = pickle.load(open('model.pkl', 'rb'))
-count_vect = CountVectorizer()
+model = pickle.load(open('model new.pkl', 'rb'))
+count_vect = TfidfVectorizer(ngram_range=(1,2),
+                
+                     token_pattern=u'(?ui)\\b\\w*[a-z]+\\w*[a-z]+\\w*\\b')
 final_features = str(sys.argv[1])
-prediction = model.predict(count_vect.fit_transform([final_features]))
+final_features = [final_features]
+count_vect.fit(final_features)
+count_vect_dtm = count_vect.transform(final_features)
+count_vect_dense = pd.DataFrame(count_vect_dtm.toarray(), columns = count_vect.get_feature_names())
+pca = PCA(n_components = 1) # put a reasonable number from graph (70%-90%)
+count_vect_pca = pca.fit_transform(count_vect_dense)
+prediction = model.predict(count_vect_pca)
 
-if (prediction==1):
+if (prediction==2):
     prediction = 1
 else:
     prediction = 0
