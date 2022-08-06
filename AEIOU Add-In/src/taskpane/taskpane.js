05dc058e347add4a16fdf79378de7ee6c6d9f3b5
MandisLoh <mandisloh13@gmail.com>
2022-08-06 21:33:20 +0800

    opens meeting interface

--- AEIOU Add-In/src/taskpane/taskpane.js
+++ AEIOU Add-In/src/taskpane/taskpane.js
@@ -18,8 +18,32 @@ export async function run() {
    * Insert your Outlook code here
    */
   // Get a reference to the current message
-  const item = Office.context.mailbox.item;
+  // const item = Office.context.mailbox.item
+  Office.context.mailbox.item.body.getAsync(
+    "text",
+    { asyncContext: "This is passed to the callback" },
+    function callback(result) {
+        document.getElementById("item-subject").innerHTML = "<b>Subject:</b> <br/>" + result.value;
+        
+        // Put in model to use the result.value
+        
+        //result = Result of running model
+        const start = new Date();
+        const end = new Date();
+        end.setHours(start.getHours() + 1);
 
+        //if (result){displayNewAppointmentForm}
+        Office.context.mailbox.displayNewAppointmentForm({
+          start: start,
+          end: end,
+          location: "Home",
+          subject: "meeting",
+          body: result.value
+        });
+        //else{document.getElementById("item-subject").innerHTML = Not a meeting!}
+    });
+  
   // Write message property value to the task pane
-  document.getElementById("item-subject").innerHTML = "<b>Subject:</b> <br/>" + item.subject;
+  // document.getElementById("item-subject").innerHTML = "<b>Subject:</b> <br/>" + item.body;
+  // document.body.getElementsByClassName("foo");
 }
