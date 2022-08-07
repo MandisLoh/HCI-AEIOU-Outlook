f8472c9f7df7f658752c3e7d10e3216813c3a50b
MandisLoh <mandisloh13@gmail.com>
2022-08-07 20:10:32 +0800

    Revert "editing compose part"
    This reverts commit a5ee7456ebbceafaf20bf44062f1a5d47aad48a6.

--- AEIOU Add-In/src/launchevent/launchevent.js
+++ AEIOU Add-In/src/launchevent/launchevent.js
@@ -38,7 +38,7 @@ function onMessageSendHandler(event) {
       return false;
     }
   
-    const arrayOfTerms = ["meeting", "meet", "meets", "magic"];
+    const arrayOfTerms = ["send", "picture", "document", "attachment"];
     for (let index = 0; index < arrayOfTerms.length; index++) {
       const term = arrayOfTerms[index].trim();
       const regex = RegExp(term, 'i');
@@ -60,9 +60,9 @@ function onMessageSendHandler(event) {
         }
       }
   
-      event.completed({ allowEvent: false, errorMessage: "Looks like you're sending a meeting invite! Have you saved it to calendar?" });
+      event.completed({ allowEvent: false, errorMessage: "Looks like you forgot to include an attachment?" });
     } else {
-      event.completed({ allowEvent: false, errorMessage: "Looks like you're sending a meeting invite! Have you saved it to calendar?" });
+      event.completed({ allowEvent: false, errorMessage: "Looks like you're forgetting to include an attachment?" });
     }
   }
   
