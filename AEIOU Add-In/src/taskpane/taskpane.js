b2bcf2bf4d0dfdcbc1e814e03e5ae7550b602f34
MandisLoh <mandisloh13@gmail.com>
2022-08-07 18:25:59 +0800

    meeting popup successful

--- AEIOU Add-In/src/taskpane/taskpane.js
+++ AEIOU Add-In/src/taskpane/taskpane.js
@@ -5,6 +5,9 @@
 
 /* global document, Office */
 
+
+
+
 Office.onReady((info) => {
   if (info.host === Office.HostType.Outlook) {
     document.getElementById("sideload-msg").style.display = "none";
@@ -14,83 +17,76 @@ Office.onReady((info) => {
 });
 
 export async function run() {
-  /**
-   * Insert your Outlook code here
-   */
-  // Get a reference to the current message
-  // const item = Office.context.mailbox.item
+  
+
+
   Office.context.mailbox.item.body.getAsync(
     "text",
     { asyncContext: "This is passed to the callback" },
-    function callback(result) {
-                
-        // const spawn = require('child_process').spawn;
-        // const script = spawn('py', ['ml copy 2.py', result.value]);
-        // console.log(result.value)
+    function callback(result)  {
+        document.getElementById("item-subject").innerHTML = "<b>Body:</b> <br/>" + result.value;
+        var input = result.value;
+        console.log(input);
 
-        // script.stdout.on('data', (data) => {
-        //     // datatoSend = data.toString();
-        //     console.log(`${data}`)
-        // });
-        // script.stderr.on('data', (data) => {
-        //     // As said before, convert the Uint8Array to a readable string.
-        //     console.error(`stderr: ${data}`);
-        // });
+        // remove all punctuation
+        var replaced = input.replace(/[.,\/#!$%\^&\*;:{}=\-_`~()""]/g," ");
+        console.log(replaced);
+        // changes the consecutive empty spaces into only 1 empty space
+        var replaced1 = replaced.replace(/\s{2,}/g,' ');
+        console.log(replaced1);
+        // replace empty space with +
+        var replaced2 = replaced1.slice(0, -1).split(' ').join('+') + replaced1.slice(-1);
+        console.log(replaced2);
+       
+        // heroku website
+        var predicted = 'https://outlook-addin-aeiou.herokuapp.com/predict?textbody=' + replaced2.toLowerCase() ;
+        console.log(predicted) ;
+        
+        //get predicted value
+        var xmlHttp = new XMLHttpRequest();
+        xmlHttp.open( "GET", predicted, false ); // false for synchronous request
+        xmlHttp.send( null );
+        var returned_prediction = xmlHttp.responseText;
+        console.log(returned_prediction);
+        
+        if (returned_prediction = {"Prediction":"Meeting"}) {
+        const start = new Date();
+        const end = new Date();
+        end.setHours(start.getHours() + 1);
+        Office.context.mailbox.displayNewAppointmentForm({
+                start: start,
+                end: end,
+                location: "",
+                subject: Office.context.mailbox.item.subject,
+                body: result.value
+              })}         
 
-        // script.on('close', (code) => {
-        //     console.log("Process quit with code : " + code);
-        //     // res.send(datatoSend);
-        // });
+        else {document.getElementById("item-subject").innerHTML = "Not a meeting!"}
+          ; }
+  )}
+        
+      
 
-        // Setting up tfjs with the model we downloaded
-        tf.loadLayersModel('model.json')
-        .then(function (model) {
-            window.model = model;
-            console.log("window model")
-        });
-
-        // Predict function
-        let predicted = 0;
-        var predict = function (input) {
-        if (window.model) {
-            window.model.predict([tf.tensor(input)
-                .reshape([1, 28, 28, 1])])
-                .array().then(function (scores) {
-                    scores = scores[0];
-                    predicted = scores
-                        .indexOf(Math.max(...scores));
-                    $('#number').html(predicted);
-                    console.log(predicted)
-                });
-        } else {
-
-            // The model takes a bit to load, 
-            // if we are too fast, wait
-            setTimeout(function () { predict(input) }, 50);
-        }
-        }
-
-        document.getElementById("item-subject").innerHTML = "<b>Subject:</b> <br/>" + result.value;
+        
         
         // Put in model to use the result.value
         
         //result = Result of running model
-        const start = new Date();
-        const end = new Date();
-        end.setHours(start.getHours() + 1);
+    //     const start = new Date();
+    //     const end = new Date();
+    //     end.setHours(start.getHours() + 1);
 
-        //if (result){displayNewAppointmentForm}
-        Office.context.mailbox.displayNewAppointmentForm({
-          start: start,
-          end: end,
-          location: "",
-          subject: "",
-          body: result.value
-        });
-        //else{document.getElementById("item-subject").innerHTML = Not a meeting!}
-    });
+    //     //if (result){displayNewAppointmentForm}
+    //     Office.context.mailbox.displayNewAppointmentForm({
+    //       start: start,
+    //       end: end,
+    //       location: "",
+    //       subject: "",
+    //       body: result.value
+    //     });
+    //     //else{document.getElementById("item-subject").innerHTML = Not a meeting!}
+  // });
   
   // Write message property value to the task pane
   // document.getElementById("item-subject").innerHTML = "<b>Subject:</b> <br/>" + item.body;
-  // document.body.getElementsByClassName("foo");
-}
+  //document.body.getElementsByClassName("foo")
--- AEIOU Add-In/taskpane.js
+++ AEIOU Add-In/taskpane.js
@@ -12,84 +12,105 @@ Office.onReady((info) => {
   }
 });
 
-export async function run() {
+
   /**
    * Insert your Outlook code here
    * 
    */
-  const spawn = require('child_process').spawn;
-  console.log("input")
-  const script = spawn('python.exe', ['ml copy 2.py', input]);
-  console.log(input)
+  // const spawn = require('child_process').spawn;
+  // console.log("input")
+  // const script = spawn('python.exe', ['ml copy 2.py', input]);
+  // console.log(input)
   
-  script.stdout.on('data', (data) => {
-    // datatoSend = data.toString();
-    console.log(`${data}`)
-  });
-  script.stderr.on('data', (data) => {
-    // As said before, convert the Uint8Array to a readable string.
-    console.error(`stderr: ${data}`);
-  });
+  // script.stdout.on('data', (data) => {
+  //   // datatoSend = data.toString();
+  //   console.log(`${data}`)
+  // });
+  // script.stderr.on('data', (data) => {
+  //   // As said before, convert the Uint8Array to a readable string.
+  //   console.error(`stderr: ${data}`);
+  // });
   
-  script.on('close', (code) => {
-    console.log("Process quit with code : " + code);
-  });
+  // script.on('close', (code) => {
+  //   console.log("Process quit with code : " + code);
+  // });
    /* global document, Office */
   // Get a reference to the current message
   // const item = Office.context.mailbox.item
+
+  export async function run() {
   Office.context.mailbox.item.body.getAsync(
     "text",
     { asyncContext: "This is passed to the callback" },
     function callback(result) {
         document.getElementById("item-subject").innerHTML = "<b>Body:</b> <br/>" + result.value;
         
-        return result.value },
+        input = result.value
   
-        callml(result.value),
         
+   
+        // Put in model to use the result.value
+        tf.loadLayersModel('model.json')
+        .then(function (model) {
+            window.model = model;
+            console.log("window model")
+        });
+        
+        //result = Result of running model
+        const start = new Date();
+        const end = new Date();
+        end.setHours(start.getHours() + 1);
 
-    function callml(input) {   
-      const spawn = require('child_process').spawn;
-      console.log("input")
-      const script = spawn('python.exe', ['ml copy 2.py', input]);
-      console.log(input)
+        //if (result){displayNewAppointmentForm}
+        Office.context.mailbox.displayNewAppointmentForm({
+          start: start,
+          end: end,
+          location: "",
+          subject: "",
+          body: result.value
+        });
+        //else{document.getElementById("item-subject").innerHTML = Not a meeting!}
+    });
   
-      script.stdout.on('data', (data) => {
-        // datatoSend = data.toString();
-        console.log(`${data}`)
-      });
-      script.stderr.on('data', (data) => {
-        // As said before, convert the Uint8Array to a readable string.
-        console.error(`stderr: ${data}`);
-      });
+        }
+
+    // function callml(input) {   
+    //   const spawn = require('child_process').spawn;
+    //   console.log("input")
+    //   const script = spawn('python.exe', ['ml copy 2.py', input]);
+    //   console.log(input)
   
-      script.on('close', (code) => {
-        console.log("Process quit with code : " + code);
+    //   script.stdout.on('data', (data) => {
+    //     // datatoSend = data.toString();
+    //     console.log(`${data}`)
+    //   });
+    //   script.stderr.on('data', (data) => {
+    //     // As said before, convert the Uint8Array to a readable string.
+    //     console.error(`stderr: ${data}`);
+    //   });
+  
+    //   script.on('close', (code) => {
+    //     console.log("Process quit with code : " + code);
         // res.send(datatoSend);
       
         
-      const start = new Date();
-      const end = new Date();
-      end.setHours(start.getHours() + 1);
-
-      Office.context.mailbox.displayNewAppointmentForm({
-        requiredAttendees: ["bob@contoso.com"],
-        optionalAttendees: ["sam@contoso.com"],
-        start: start,
-        end: end,
-        location: "Home",
-        subject: "meeting",
-        resources: ["projector@contoso.com"],
-        body: result.value
-      });
+      
+    //   });
+  
+    //   script.on('close', (code) => {
+    //     console.log("Process quit with code : " + code);
+        // res.send(datatoSend);
+      
+        
+      
           
           
           
-    });  } 
+    
         
     
 
-  )
+  
         // Put in model to use the result.value
         
 
@@ -115,7 +136,7 @@ export async function run() {
   // Write message property value to the task pane
   // document.getElementById("item-subject").innerHTML = "<b>Subject:</b> <br/>" + item.body;
   // document.body.getElementsByClassName("foo");
-}
+
 
 
 
